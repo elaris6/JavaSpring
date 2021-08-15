8944e81cdf2617ffdcc863f442490a197c8cf581
Izzrra <elaris6@gmail.com>
2021-08-15 09:44:12 +0200

    Anotaciones extra sobre consultas con convenciones y personalizadas en Spring JPA. Notas en modulo14, UsuarioDAO

--- 14ControlClientesSpringBootstrap/src/main/java/es/local/personal/dao/UsuarioDAO.java
+++ 14ControlClientesSpringBootstrap/src/main/java/es/local/personal/dao/UsuarioDAO.java
@@ -14,7 +14,32 @@ public interface UsuarioDAO extends JpaRepository<Usuario, Integer>{
     /* Aunque la herencia de "JpaRepository<E,T>" incluye todos los métodos CRUD básicos,
     es posible incluir los métodos personalizados que sean necesarios de forma adicional. */
     
+    /* Ejemplo de método de búsqueda por un campo, usando las conveciones de Spring JPA.
+    Hay bastantes convenciones ya creadas que permiten realizar queries sin necesidad de
+    escribir el código JPQL. */
     
+    /* Búsqueda por un campo concreto. El nombre del campo en el método deb ser exactamente
+    igual que en la clase entidad.*/
     public Usuario findByUsername(String username);
+    /* Búsqueda combinada por dos campos. */
+    public Usuario findByUsernameAndPassword(String username, String password);
+    /* Búsqueda por un campo que contenga un substring ignorando además las mayúsc/minusc. */
+    public List<Usuario> findUsernameContainingIgnoreCase(String cadena);
     
+    
+    /* Definición de método con JPQL personalizado. */
+    @Query("SELECT u FROM Usuario u WHERE u.username LIKE %:cadena%")
+    public List<Usuario> buscarUsuarioPorTexto(@Param("cadena") String cadena);
+    /* Misma consulta con parámetros ordenados. */
+    //@Query("SELECT u FROM Usuario u WHERE u.username LIKE ?1%")
+    //public List<Usuario> buscarUsuarioPorTexto(String cadena);
+    
+    
+    /* Definición de método transaccional personalizado.
+    Este método debe anotarse con "Transactional" and "Modifying", para que la operación
+    se realice de manera consistente al ser una modificación de la BBDD. */
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Usuario u WHERE u.password = ?1%")
+    public void eliminarPorPassword(String password)
 }
